20f19dc7a95bd70c23806f3c944532cfe4a8776b
Rick Davidson <rd_187@yahoo.com>
2019-05-27 11:14:50 -0400

    Firt commit

---
+++ setup-git-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Rick Davidson	
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "Rick Davidson"
+git config --global user.email "rd_187@yahoo.com"
+sudo git config --system core.editor vim
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
